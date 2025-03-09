b1f06a055c74f78c6a26a1edce7b3745d4108904
prahladkumar159 <42700353+prahladkumar159@users.noreply.github.com>
2025-03-09 17:10:29 +0530

    Exam
    Exam property

---
+++ mcqQSController.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OnlineExam.Data;
+using OnlineExam.Models;
+using Microsoft.AspNetCore.Http;
+
+
+namespace OnlineExam.Controllers
+{
+    public class mcqQSController : Controller
+    {
+        private readonly OnlineExamContext _context;
+        private  object SessionName;
+        public mcqQSController(OnlineExamContext context)
+        {
+            _context = context;
+        }
+
+        // GET: mcqQS
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.mcqQS.ToListAsync());
+        }
+
+        // GET: mcqQS/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mcqQS = await _context.mcqQS
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (mcqQS == null)
+            {
+                return NotFound();
+            }
+
+            return View(mcqQS);
+        }
+
+        public async Task<IActionResult> Exam(int? ids)
+        {
+            if (ids == null)
+            {
+                ids = 1;
+            }
+
+            var mcqQS = await _context.mcqQS
+                .FirstOrDefaultAsync(m => m.id == ids);
+            mcqQS.ans = "1";
+            if (mcqQS != null)
+            {
+                HttpContext.Session.SetString(SessionName, mcqQS);
+            }
+            if (mcqQS == null)
+            {
+                return NotFound();
+            }
+            TempData["id"] = ids;
+            return View(mcqQS);
+        }
+        [HttpPost]
+        public IActionResult Next(mcqQS mcqQS)
+        {
+            var id=Convert.ToInt32(TempData["id"])+1;
+            return RedirectToAction("Exam",new { ids = id });
+        }
+        public IActionResult Previous()
+        {
+            var id =Convert.ToInt32(TempData["id"]);
+            return RedirectToAction("Exam", new { ids = id-1 });
+        }
+        // GET: mcqQS/Create
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: mcqQS/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,course,qsId,qsNo,qs,op1,op2,op3,op4,ans,tag,eTime")] mcqQS mcqQS)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(mcqQS);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mcqQS);
+        }
+
+        // GET: mcqQS/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mcqQS = await _context.mcqQS.FindAsync(id);
+            if (mcqQS == null)
+            {
+                return NotFound();
+            }
+            return View(mcqQS);
+        }
+
+        // POST: mcqQS/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("id,course,qsId,qsNo,qs,op1,op2,op3,op4,ans,tag,eTime")] mcqQS mcqQS)
+        {
+            if (id != mcqQS.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(mcqQS);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!mcqQSExists(mcqQS.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(mcqQS);
+        }
+
+        // GET: mcqQS/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var mcqQS = await _context.mcqQS
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (mcqQS == null)
+            {
+                return NotFound();
+            }
+
+            return View(mcqQS);
+        }
+
+        // POST: mcqQS/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var mcqQS = await _context.mcqQS.FindAsync(id);
+            if (mcqQS != null)
+            {
+                _context.mcqQS.Remove(mcqQS);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool mcqQSExists(int id)
+        {
+            return _context.mcqQS.Any(e => e.id == id);
+        }
+    }
+}
---
+++ userInfoesController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using OnlineExam.Data;
+using OnlineExam.Models;
+
+namespace OnlineExam.Controllers
+{
+    public class userInfoesController : Controller
+    {
+        private readonly OnlineExamContext _context;
+
+        public userInfoesController(OnlineExamContext context)
+        {
+            _context = context;
+        }
+
+        // GET: userInfoes
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var x = Convert.ToDouble(await _context.userInfo.ToListAsync());
+                return View(x);
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        // GET: userInfoes/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userInfo = await _context.userInfo
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            return View(userInfo);
+        }
+
+        // GET: userInfoes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: userInfoes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("id,name,department,email,semester,gender,password,fatherName,hall,image,no_of_exam,total_mark,rank,abc,def")] userInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userInfo);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userInfo);
+        }
+
+        // GET: userInfoes/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userInfo = await _context.userInfo.FindAsync(id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+            return View(userInfo);
+        }
+
+        // POST: userInfoes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("id,name,department,email,semester,gender,password,fatherName,hall,image,no_of_exam,total_mark,rank,abc,def")] userInfo userInfo)
+        {
+            if (id != userInfo.id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userInfo);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!userInfoExists(userInfo.id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userInfo);
+        }
+
+        // GET: userInfoes/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userInfo = await _context.userInfo
+                .FirstOrDefaultAsync(m => m.id == id);
+            if (userInfo == null)
+            {
+                return NotFound();
+            }
+
+            return View(userInfo);
+        }
+
+        // POST: userInfoes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var userInfo = await _context.userInfo.FindAsync(id);
+            if (userInfo != null)
+            {
+                _context.userInfo.Remove(userInfo);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool userInfoExists(string id)
+        {
+            return _context.userInfo.Any(e => e.id == id);
+        }
+    }
+}
